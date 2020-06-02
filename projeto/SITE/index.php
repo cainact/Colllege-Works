c8294b01a175a88c67c5c9d9adfa29c25adff414
lucastravesso <61063875+lucastravesso@users.noreply.github.com>
2020-06-02 20:07:58 -0300

    Create index.php

---
+++ projeto/SITE/index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="..\project\CSS\styles.css" type="text/css">
+
+
+</head>
+<body>
+
+    
+    <div class="header">
+        <h1>Mogi Transparente</h1>
+        <h2>Seu site de buscas politicas</h2>
+        <h3>Para saber os salarios e propostas dos funcionarios publicos basta procurar <br> por nome ou cargo e sera 
+        exibido uma lista com as informações. </h3>
+    </div>
+    
+    <p class="info"> = Pesquisar</p>
+    <div class="Buscador">
+        <form method="get" action="pagina-de-busca.php">
+                <input class="box" type="text" name="pesquisar" id="textoDeBusca" placeholder="Pesquisar . . .">
+                <button type="submit" class="btn-img btn-loc"></button>     
+        </form>
+    </div>
+    <div>
+        
+    </div>
+            <p class="titulo-pesquisas">Pesquisas de exemplo ..</p>  
+           
+                <li><a class="pesquisa-pronta">Quem ganha mais ?</a></li>
+                <li><a class="pesquisa-pronta2">Projetos mais relevantes</a></li>
+                <li><a class="pesquisa-pronta3">Salario do prefeito</a></li>
+
+
+</body>
+</html>
+                        
